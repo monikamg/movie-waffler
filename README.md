9969dad80be5017d228b78afcd8af718cb958c4a
Monika M G <mgmstuff@gmail.com>
2020-07-18 13:07:50 +0530

    Commiting the whole project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Angular Movies | Movies
+
+Angular Movies & TV Shows was generated with:
+- Angular CLI version 8.3.0
+- Angular 8.2.3
+- Angular Material 8.1.4
+- AngularFire 5.2.1
+- Firebase js SDK 6.4.2
+- Rxjs 6.5.2
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'YOUR-FIREBASE-DATA',
+    authDomain: 'YOUR-FIREBASE-DATA',
+    databaseURL: 'YOUR-FIREBASE-DATA',
+    projectId: 'YOUR-FIREBASE-DATA',
+    storageBucket: 'YOUR-FIREBASE-DATA',
+    messagingSenderId: 'YOUR-FIREBASE-DATA'
+  }
+};
